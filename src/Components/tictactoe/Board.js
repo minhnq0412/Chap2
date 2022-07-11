b7534ef83907047036667b1f26bcd561158b9305
minhnq0412 <raizel41200@gmail.com>
2022-07-11 17:54:46 +0700

    minhnq commit

--- src/Components/tictactoe/Board.js
+++ src/Components/tictactoe/Board.js
@@ -2,7 +2,7 @@ import React from "react";
 import { winner } from "../helpers";
 import Cell from "./Cell";
 
-const Board = (props) => {
+const Board = ({cells,onClick}) => {
   //
   // const cells = [null, null, null, "Y", "X", "X", null, null, null];
   // console.log();
@@ -10,8 +10,8 @@ const Board = (props) => {
 
   return (
     <div className="game-board">
-      {props.cells.map((o, i) => (
-        <Cell key={i} value={o} onClick={() => props.onClick(i)}></Cell>
+      {cells.map((o, i) => (
+        <Cell key={i} value={o} onClick={() => onClick(i)}></Cell>
       ))}
     </div>
   );
